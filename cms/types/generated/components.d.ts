2fb8dabad924bae96b2c8aeaf53ffd536dea280c
CouriusDeveloper <153290946+CouriusDeveloper@users.noreply.github.com>
2025-12-02 21:08:40 +0000

    feat: entferne nicht verwendete Komponenten und aktualisiere Typdefinitionen

--- cms/types/generated/components.d.ts
+++ cms/types/generated/components.d.ts
@@ -1,31 +1,3 @@
-import type { Schema, Struct } from '@strapi/strapi';
-
-export interface SharedScheduleEntry extends Struct.ComponentSchema {
-  collectionName: 'components_shared_schedule_entries';
-  info: {
-    displayName: 'Schedule Entry';
-  };
-  attributes: {
-    activity: Schema.Attribute.String & Schema.Attribute.Required;
-    time: Schema.Attribute.String & Schema.Attribute.Required;
-  };
-}
-
-export interface SharedTextItem extends Struct.ComponentSchema {
-  collectionName: 'components_shared_text_items';
-  info: {
-    displayName: 'Text Item';
-  };
-  attributes: {
-    value: Schema.Attribute.String & Schema.Attribute.Required;
-  };
-}
-
-declare module '@strapi/strapi' {
-  export module Public {
-    export interface ComponentSchemas {
-      'shared.schedule-entry': SharedScheduleEntry;
-      'shared.text-item': SharedTextItem;
-    }
-  }
-}
+/*
+ * The app doesn't have any components yet.
+ */
