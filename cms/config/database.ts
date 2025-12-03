6f4af96740834595485fa1c6dfd0f1073d9037c5
CouriusDeveloper <153290946+CouriusDeveloper@users.noreply.github.com>
2025-12-03 17:25:53 +0000

    feat: entferne PostgreSQL-Unterstützung aus der Datenbankkonfiguration und aktualisiere Abhängigkeiten

--- cms/config/database.ts
+++ cms/config/database.ts
@@ -1,36 +1,12 @@
 import path from 'path'
 
-export default ({ env }) => {
-  const client = env('DATABASE_CLIENT', 'sqlite')
-
-  const connections = {
-    sqlite: {
-      client: 'sqlite',
-      connection: {
-        filename: path.join(__dirname, '..', '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-      },
-      useNullAsDefault: true,
+export default ({ env }) => ({
+  connection: {
+    client: 'sqlite',
+    connection: {
+      filename: path.join(__dirname, '..', '..', env('DATABASE_FILENAME', '.tmp/data.db')),
     },
-    postgres: {
-      client: 'postgres',
-      connection: {
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi'),
-        user: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
-        ssl: env.bool('DATABASE_SSL', true)
-          ? { rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false) }
-          : false,
-      },
-    },
-  }
-
-  if (!(client in connections)) {
-    throw new Error(`Unsupported database client: ${client}`)
-  }
-
-  return {
-    connection: connections[client],
-  }
-}
+    useNullAsDefault: true,
+    debug: false,
+  },
+})
