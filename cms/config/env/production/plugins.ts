d960ed43326940d901e62dcba44b315d561873ac
CouriusDeveloper <153290946+CouriusDeveloper@users.noreply.github.com>
2025-12-17 17:02:41 +0000

    feat: füge Mailgun-Plugin-Konfiguration für E-Mail-Dienste hinzu

---
+++ cms/config/env/production/plugins.ts
@@ -0,0 +1,16 @@
+export default ({ env }) => ({
+  email: {
+    config: {
+      provider: 'mailgun',
+      providerOptions: {
+        key: env('MAILGUN_API_KEY'),
+        domain: env('MAILGUN_DOMAIN'),
+        url: env('MAILGUN_API_URL', 'https://api.mailgun.net'),
+      },
+      settings: {
+        defaultFrom: env('MAIL_FROM', 'info@kalikicker.de'),
+        defaultReplyTo: env('MAIL_REPLY_TO', 'info@kalikicker.de'),
+      },
+    },
+  },
+})
